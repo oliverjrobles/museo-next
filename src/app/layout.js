edf0f20fd4bbeeda3ce150650dc86eda6d765bd6
oliverjrobles <oliver.robles@live.dk>
2025-12-06 02:44:24 +0100

    hero section

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,14 @@
 import "./globals.css";
-import Navbar from "./components/Navbar";
 
 export const metadata = {
-  title: "Museo",
-  description: "Nightclub website",
+  title: "Nightclub",
+  description: "Have a good time",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        <Navbar />
-        {children}
-      </body>
+      <body className="bg-black text-white">{children}</body>
     </html>
   );
 }
