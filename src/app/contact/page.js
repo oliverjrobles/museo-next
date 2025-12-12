64802d03db89c7e2cf41e4a43d329af53c8c7cde
oliverjrobles <oliver.robles@live.dk>
2025-12-12 19:54:04 +0100

    contact us og ændrer

---
+++ src/app/contact/page.js
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar";
+import ContactHero from "./ContactHero";
+import ContactForm from "./ContactForm";
+
+export default function ContactPage() {
+  return (
+    <>
+      <Navbar />
+      <ContactHero />
+      <ContactForm />
+    </>
+  );
+}
