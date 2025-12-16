56c39690639b27c36e43d0413746ea49de872ba4
oliverjrobles <oliver.robles@live.dk>
2025-12-16 22:17:58 +0100

    zod til book table

--- src/app/book/page.js
+++ src/app/book/page.js
@@ -1,15 +1,20 @@
+"use client";
+import { useState } from "react";
+
 import Navbar from "../components/Navbar";
 import BookTableHero from "./BookTableHero";
 import Tables from "./Tables";
 import BookingForm from "./BookingForm";
 
 export default function BookPage() {
+  const [selectedTable, setSelectedTable] = useState(null);
+
   return (
     <>
       <Navbar />
       <BookTableHero />
-      <Tables />
-      <BookingForm />
+      <Tables selectedTable={selectedTable} onSelectTable={setSelectedTable} />
+      <BookingForm selectedTable={selectedTable} />
     </>
   );
 }
